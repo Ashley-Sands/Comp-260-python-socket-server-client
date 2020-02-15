cecdeaea3f3e35de4745d0ffa98c6eb1345400da
Ashley Sands <ashsands1@amstrike.com>
2020-02-15 23:10:00 +0000

    got text commutication working with socket, also fixed gitinore

---
+++ unity client/simpleSocket/Assets/Scripts/WebClient/Test/Test_Receive.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Receive : MonoBehaviour
+{
+	[SerializeField] private TMPro.TextMeshProUGUI textOutput;
+
+	private void Update ()
+	{
+		string message = SocketClient.ActiveSocket.GetMessage();
+		if (message.Length > 0)
+		{
+			print( message.Length + " ~~~~ " + message[ message.Length - 1 ] );
+
+			string text = textOutput.text.ToString();
+			Debug.Log( "fdsf@@ "+text );
+			Debug.LogWarning( "@@@"+ message + text );
+			Debug.Log( "###"+text );
+
+			textOutput.text = message +"\n"+ text;
+
+		}
+
+	}
+
+}
---
+++ unity client/simpleSocket/Assets/Scripts/WebClient/Test/Test_sendMessage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Test_sendMessage : MonoBehaviour
+{
+
+	[SerializeField] private TMP_InputField textInput;
+
+	public void SendMessageToServer()
+	{
+
+		SocketClient.ActiveSocket.SetMessage( textInput.text );
+
+		textInput.text = "";
+
+	}
+
+}
